767c9d47a3de9746ebfebc7d6c8fb797c74b25a6
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-03-31 01:39:35 +0530

    new Dashboard UI

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import AddGoalModal from '../components/modal/AddGoalModal';
+import { AiOutlinePlus, AiFillEdit } from 'react-icons/ai'
+
+import GoalCard from '../components/card/GoalCard';
 
 const Dashboard = () => {
+    const [isAddGoalModalOpen, setIsAddGoalModalOpen] = useState(false);
+    const navigate = useNavigate();
+    const { user } = useSelector(state => state.auth);
+
+    const toggleAddGoalModal = () => {
+        setIsAddGoalModalOpen(!isAddGoalModalOpen);
+    }
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/');
+        }
+    }, [navigate, user])
+
     return (
-        <div>Dashboard</div>
+        <React.Fragment>
+            <div className='my-5'>
+                <div className='flex items-center justify-between'>
+                    <h1 className='text-lg md:text-2xl'>Dashboard</h1>
+                    <button
+                        onClick={toggleAddGoalModal}
+                        className='bg-blue-500 py-3 px-3 rounded-md text-white flex items-center gap-3'
+                    >
+                        <AiOutlinePlus />
+                        Add Goal
+                    </button>
+                </div>
+                <div className='goals-container'>
+                    <h1 className='text-lg md:text-3xl'>All Goals</h1>
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-2 my-3'>
+
+                        {
+                            [1, 2, 3, 4, 5, 6, 7].map((_, idx) => {
+                                return (
+                                    <GoalCard key={idx} />
+                                );
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+            <AddGoalModal
+                toggleAddGoalModal={toggleAddGoalModal}
+                isAddGoalModalOpen={isAddGoalModalOpen}
+            />
+        </React.Fragment>
     )
 }
 
